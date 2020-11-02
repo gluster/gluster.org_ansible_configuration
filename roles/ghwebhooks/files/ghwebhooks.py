6390e40b81324e5c93a9bc7592b368077986e6aa
Michael Scherer <misc@redhat.com>
2020-11-02 23:48:17 +0100

    Add a service to handle github hooks
    Nothing fancy for now, just want to send email on issue on project infra repo
    Fow now, it send nothing, will see later

---
+++ roles/ghwebhooks/files/ghwebhooks.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, Response
+import os
+import hmac
+
+app = Flask(__name__)
+
+# "It's a secret to everybody"
+webhook_secret = os.getenv('SECRET')
+
+# https://ryanharrison.co.uk/2018/09/04/validate-github-webhook-signatures.html
+@app.route('/', methods=['POST', 'GET'])
+def respond():
+    if request.method == 'GET':
+        return Response(status=200)
+    if not "X-Hub-Signature" in request.headers:
+        abort(400) # bad request if no header present
+
+    if webhook_secret:
+        signature = request.headers['X-Hub-Signature']
+        payload = request.data
+        secret = webhook_secret.encode()
+    
+        hmac_gen = hmac.new(secret, payload, hashlib.sha1)
+
+        digest = "sha1=" + hmac_gen.hexdigest()
+        if not hmac.compare_digest(digest, signature):
+            abort(400) # if the signatures don't match, bad request not from GitHub
+
+    event = request.headers['X-Github-Event']
+    if event != 'issue_comment':
+        return Response(status=200)
+
+    j = request.json
+    if j['action'] != 'created':
+        return Response(status=200)
+
+    if j['repository'] != "gluster/project-infrastructure":
+        return Response(status=200)
+
+    print(j)
+    # TODO 
+    # check the format 
+    # send the format
+
+    return Response(status=200)
+

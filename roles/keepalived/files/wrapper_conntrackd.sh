27a35bd3a1c9a70c2525aac01be7ae7d5fea512b
Michael Scherer <misc@redhat.com>
2021-11-03 18:18:34 +0100

    Use /etc/motd.d to change the motd

--- roles/keepalived/files/wrapper_conntrackd.sh
+++ roles/keepalived/files/wrapper_conntrackd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sed -i '/^Keepalived status:.*$/d' /etc/motd
-echo "Keepalived status: $1" >> /etc/motd
+echo "Keepalived status: $1" >> /etc/motd.d/status
 
 if [ -f /usr/share/doc/conntrack-tools/doc/sync/primary-backup.sh ] ; then
     bash /usr/share/doc/conntrack-tools/doc/sync/primary-backup.sh $*

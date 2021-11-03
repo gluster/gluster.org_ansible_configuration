78794001fb14c09e708bef2b1c89575390f55a43
Michael Scherer <misc@redhat.com>
2021-11-03 18:25:21 +0100

    Fix the script

--- roles/keepalived/files/wrapper_conntrackd.sh
+++ roles/keepalived/files/wrapper_conntrackd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Keepalived status: $1" >> /etc/motd.d/status
+echo "Keepalived status: $1" > /etc/motd.d/status
 
 if [ -f /usr/share/doc/conntrack-tools/doc/sync/primary-backup.sh ] ; then
     bash /usr/share/doc/conntrack-tools/doc/sync/primary-backup.sh $*

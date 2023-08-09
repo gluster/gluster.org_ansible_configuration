45ec37baee60244215f86e9f4c5b7bc4a323d969
Michael Scherer <misc@redhat.com>
2023-08-09 15:41:33 +0200

    Add crude support for ipv6

--- roles/keepalived/templates/notify_script.sh
+++ roles/keepalived/templates/notify_script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "Keepalived status: $1" > /etc/motd.d/status
 
+{% if address_v6 is defined %}
+CMD=del
+if [[ $STATUS -eq "master" ]]; then CMD=add; fi;
+ip a $CMD {{ address_v6 }} dev {{ interface }}
+{% endif %}
+
 {% if notify_script is defined %}
 {{ notify_script }} $*
 {% endif %}
